8aac000b47c42eeb19d6b08336600f9b8b860098
Radoslav <rikotech@abv.bg>
2018-10-17 21:09:25 +0300

    Add transmitter

--- web-client-18LF326-XC8.X/modules/transmitter/transmitter.c
+++ web-client-18LF326-XC8.X/modules/transmitter/transmitter.c
@@ -4,18 +4,38 @@
 
 volatile static Transmitter_Self transmitter_self;
 
-void transmitter_init(void){
+void transmitter_init(void(*onReady)(void)){
+    SET_OnReadyCb(NULL);
     SET_State(stReady);
+    SET_CurIdx(0);
+    SET_DataSize(0);
 //    transmitter_self.state =stReady;
 }
+
+
 
 uint8_t transmitter_send(uint8_t * data, uint8_t length){
 //    SEND_Byte('R');
 //    TX1REG = 'R';
-    PIE1bits.TXIE = 1;
-    if(IS_State(stReady)){
-        return true;
+//    PIE1bits.TXIE = 1;
+    if(IS_State(stReady) && length > 0){
+        SET_State(stBusy);
+        SET_DataSize(length);
+        SET_DataPointer(data);
+        return transmitter_sendNext();
     } else {
+        return false;
+    }
+}
+
+uint8_t transmitter_sendNext(void){
+    if(GET_DataSize() > GET_CurIdx() ){
+        USART_Send(GET_DataAt(GET_CurIdx()));
+        INCR_CurIdx();
+        USART_EnableTxInter();
+        return true;
+    } else{
+        USART_DisableTxInter();
         return false;
     }
 }
